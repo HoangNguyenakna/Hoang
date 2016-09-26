7fa650b454201619c835a36444507e50a8747ac6
Hoang <hoangnguyenakna@gmail.com>
2016-09-26 09:30:23 +0700

    sdd

---
+++ element1/element1/List.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace element1
+{
+    class List<T>
+    {
+        element<T> head;
+        internal element<T> Head
+        {
+            get { return head; }
+            set { head = value; }
+        }
+        public List()
+        {}
+            public void addfirst(element<T> e)
+        {
+            e.next = head;
+            this.head = e;
+        }
+            }
+
+}
